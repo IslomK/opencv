c5b394bdef2ec1a69ab4fa263e030112e1f0ce7d
Islomk <i.islam.karimov@gmail.com>
2019-05-30 18:16:14 +0500

    Added requirements

--- face_recognition.py
+++ face_recognition.py
@@ -10,6 +10,7 @@ with open('lables.pickle', "rb") as f:
 
 
 face_cascade = cv2.CascadeClassifier('data/haarcascade_frontalface_alt.xml')
+eye_cascade = cv2.CascadeClassifier('data/haarcascade_eye.xml')
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 recognizer.read('trainer.yml')
 
@@ -19,13 +20,14 @@ while(True):
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5)
+    eyes = eye_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
     for x, y, w, h in faces:
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
 
         id_, conf = recognizer.predict(roi_gray)
-        if conf>=50 and conf<=60:
+        if conf>=40 and conf<=60:
             font = cv2.FONT_HERSHEY_SIMPLEX
             name = labels[id_]
             color = (255,255,255)
@@ -47,6 +49,9 @@ while(True):
         end_coord_y = y + h
 
         cv2.rectangle(frame, (x, y), (end_coord_x, end_coord_y), color, stroke)
+    
+    for x,y,w,h in eyes:
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 2)
 
     cv2.imshow('frame', frame)
 
